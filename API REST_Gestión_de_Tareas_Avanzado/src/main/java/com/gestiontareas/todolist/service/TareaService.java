ceb668e42502fe1e83eebd28e7c434a0c2615ca7
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-08 16:20:58 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/TareaService.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/TareaService.java
@@ -1,1 +1,24 @@
+package com.gestiontareas.todolist.service;
 
+import java.util.List;
+
+import com.gestiontareas.todolist.dto.request.TareaRequestDTO;
+import com.gestiontareas.todolist.dto.response.TareaResponseDTO;
+import com.gestiontareas.todolist.model.EstadoTarea;
+import com.gestiontareas.todolist.model.Tarea;
+
+public interface TareaService {
+	//TareaResponseDTO crear(TareaRequestDTO dto);
+
+	TareaResponseDTO crearTarea(TareaResponseDTO dto);
+	
+	List<TareaResponseDTO> listarTareasPorUsuario(Long usuarioId);
+	
+	TareaResponseDTO actualizarTarea(Long id, TareaRequestDTO dto);
+	
+	void elimarTarea(Long id);
+	
+	TareaResponseDTO cambiarEstado(Long id, EstadoTarea nuevoEstado);
+
+	
+}
