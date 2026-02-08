771eabd8ba24f72e7238f6f7ff202e97d36fecdb
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-08 16:24:43 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/UsuarioRepository.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/UsuarioRepository.java
@@ -1,1 +1,15 @@
+package com.gestiontareas.todolist.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gestiontareas.todolist.model.Usuario;
+
+@Repository // Indica que esta clase es un repositorio de Spring Data JPA
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+}
