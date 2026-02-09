2619b56a94eb97effbb62745152fd0f8e8479aff
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-09 11:35:39 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/request/TareaRequestDTO.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/request/TareaRequestDTO.java
@@ -1,1 +1,19 @@
+package com.gestiontareas.todolist.dto.request;
 
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+//Representa lo que entra por POST o PUT
+@Data // Clase DTO para las solicitudes de creación o actualización de tareas
+public class TareaRequestDTO {
+	
+	@NotBlank // El título no puede estar vacío
+	private String Titulo;
+	
+	private String descripcion;
+	
+	@NotNull // El estado no puede ser null
+	private Long usuarioId;
+
+}
