f91e7659a4e9e53e90e53f83e1853bed99e2ca31
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-09 11:38:19 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/GlobalExceptionHandler.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/GlobalExceptionHandler.java
@@ -1,1 +1,8 @@
+package com.gestiontareas.todolist.exception;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice	// Indica que esta clase maneja excepciones de manera global para los controladores REST
+public class GlobalExceptionHandler {
+
+}
