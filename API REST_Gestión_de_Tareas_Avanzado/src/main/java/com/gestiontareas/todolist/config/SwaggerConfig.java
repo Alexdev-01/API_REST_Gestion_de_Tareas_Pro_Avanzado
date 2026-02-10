7719b6226fd0f7d798cc100544c3df443e5eed56
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-10 13:08:05 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/config/SwaggerConfig.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/config/SwaggerConfig.java
@@ -1,1 +1,8 @@
+package com.gestiontareas.todolist.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration	// Indica que esta clase es una clase de configuración de Spring
+public class SwaggerConfig {
+
+}
