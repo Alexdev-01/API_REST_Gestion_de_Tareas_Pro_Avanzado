8536ffe8f01284a3f4ea67d257acbbeb0360fb09
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-12 23:58:14 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/ErrorResponse.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/ErrorResponse.java
@@ -1,1 +1,19 @@
+package com.gestiontareas.todolist.exception;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Lombok annotation to generate getters, setters, and other utility methods
+@AllArgsConstructor // Lombok annotation to generate a constructor with all fields
+@NoArgsConstructor // Lombok annotation to generate a no-argument constructor
+public class ErrorResponse { 
+	
+	private LocalDateTime timestamp;
+	private int status;
+	private String error;
+	private String message;private String path;
+	
+}
