cd3d90643e675a8386e4593fa2202e805939d14b
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-13 14:36:44 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/TareaRepository.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/TareaRepository.java
@@ -2,6 +2,8 @@ package com.gestiontareas.todolist.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +12,11 @@ import com.gestiontareas.todolist.model.Tarea;
 @Repository // Indica que esta clase es un repositorio de Spring Data JPA
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
 	
-	List<Tarea> findByUsuarioId(Long usuarioId);
+	Page<Tarea> findByUsuarioId(Long usuarioId, Pageable pageable);
 	
 	//List tareas por usuario y estado
 	List<Tarea> findByUsuarioIdAndEstado(Long usuarioId, com.gestiontareas.todolist.model.EstadoTarea estado);
+	
+	
 
 }
