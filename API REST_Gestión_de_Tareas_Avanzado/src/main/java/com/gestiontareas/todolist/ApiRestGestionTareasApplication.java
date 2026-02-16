45616e0c4603085c49a30336ca661b032d449af0
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-16 12:29:38 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/ApiRestGestionTareasApplication.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/ApiRestGestionTareasApplication.java
@@ -1,9 +1,14 @@
 package com.gestiontareas.todolist;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication	// Anotación que indica que es una aplicación Spring Boot
 public class ApiRestGestionTareasApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(ApiRestGestionTareasApplication.class, args);	// Punto de entrada de la aplicación Spring Boot
 	}
 
 }
+
