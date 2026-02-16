80af56fb4f2af8605b0708f0dbab2c9b0eab7a9f
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-16 12:30:35 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/GlobalExceptionHandler.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/GlobalExceptionHandler.java
@@ -16,19 +16,21 @@ import jakarta.servlet.http.HttpServletRequest;
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)	// Indica que este método maneja excepciones del tipo ResourceNotFoundException
+	// Este método captura las excepciones de tipo ResourceNotFoundException, crea una respuesta de error con detalles y devuelve una respuesta con el estado HTTP 404 (Not Found)
 	public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
 		ErrorResponse error = new ErrorResponse(
 				LocalDateTime.now(),
 				HttpStatus.NOT_FOUND.value(),
 				HttpStatus.NOT_FOUND.name(),
 				ex.getMessage(),
-				request.getResquestURI());
+				request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 	
-	return RespnseEntity.status(HttpStatus.NOT_FOUND).body(error);
-	
-	
+	// Maneja cualquier otra excepción genérica que no haya sido manejada por otros métodos específicos
 	@ExceptionHandler(Exception.class)	// Indica que este método maneja cualquier excepción genérica que no haya sido manejada por otros métodos específicos
+	// Este método captura cualquier excepción genérica, crea una respuesta de error con detalles y devuelve una respuesta con el estado HTTP 500 (Internal Server Error)
 	public ResponseEntity<ErrorResponse> handleGeneric(Exception ex, HttpServletRequest request) {
 		ErrorResponse error = new ErrorResponse(
 				LocalDateTime.now(),
@@ -37,11 +39,12 @@ public class GlobalExceptionHandler {
 				"Error interno el servidor: ",
 				request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.ACCEPTEDInternal_server Error).body(error);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
 				
 	}
 	
-	
+	@ExceptionHandler(MethodArgumentNotValidException.class)	// Indica que este método maneja excepciones del tipo MethodArgumentNotValidException, que se lanzan cuando la validación de los argumentos de un método falla
+	// Este método captura las excepciones de validación, extrae los errores de validación y devuelve una respuesta con un mapa de campos y mensajes de error
 	public ResponseEntity<Map<String, String>> handleValiadationErros(MethodArgumentNotValidException ex) {
 		
 		Map<String, String> errors = new HashMap<>();
@@ -52,6 +55,4 @@ public class GlobalExceptionHandler {
 		
 		return ResponseEntity.badRequest().body(errors);
 	}
-}
-
 }
