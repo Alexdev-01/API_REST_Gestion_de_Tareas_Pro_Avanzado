bc02588478c5b2388233f0144c24ab9b32426c89
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-17 11:42:33 +0100

    Add files via upload

---
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/response/UsuarioResponseDTO.java
@@ -0,0 +1,15 @@
+package com.gestiontareas.todolist.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioResponseDTO {
+
+	private Long id;
+	private String nombre;
+	private String email;
+}
