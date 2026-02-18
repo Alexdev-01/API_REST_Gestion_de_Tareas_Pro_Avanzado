2ed43cc7334154f5aa13e0f634e032277243e705
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-18 13:07:36 +0100

    Add files via upload

---
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/request/FiltroTareaDTO.java
@@ -0,0 +1,13 @@
+package com.gestiontareas.todolist.dto.request;
+
+import com.gestiontareas.todolist.model.EstadoTarea;
+
+import lombok.Data;
+
+@Data  // Anotación de Lombok para generar getters, setters, toString, equals y hashCode automáticamente
+public class FiltroTareaDTO {
+	private EstadoTarea estado;
+	private Long usuarioId;
+	private String titulo;
+
+}
