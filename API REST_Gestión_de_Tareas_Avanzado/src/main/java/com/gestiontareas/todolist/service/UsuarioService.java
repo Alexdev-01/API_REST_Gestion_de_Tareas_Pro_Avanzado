e0d4dc02d369bd789ac85caa90f58c2c1041f42d
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-19 12:27:34 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/UsuarioService.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.gestiontareas.todolist.service;
 
+import com.gestiontareas.todolist.dto.request.UsuarioRequestDTO;
+import com.gestiontareas.todolist.dto.response.UsuarioResponseDTO;
+
 public interface UsuarioService {
 
 	UsuarioResponseDTO crearUsuario(UsuarioRequestDTO request);
