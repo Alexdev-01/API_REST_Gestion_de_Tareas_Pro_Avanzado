c675ecfc1af26415078595cc2c98581631803839
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-20 11:53:27 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/response/UsuarioResponseDTO.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/response/UsuarioResponseDTO.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Data	// Genera getters, setters, toString, equals y hashCode automáticamente
+@AllArgsConstructor	// Genera un constructor con todos los campos como parámetros
+@NoArgsConstructor	// Genera un constructor sin parámetros
+// DTO de respuesta para el usuario, utilizado para enviar datos al cliente sin exponer información sensible
 public class UsuarioResponseDTO {
 
 	private Long id;
 	private String nombre;
 	private String email;
+	
+	
+	
 }
