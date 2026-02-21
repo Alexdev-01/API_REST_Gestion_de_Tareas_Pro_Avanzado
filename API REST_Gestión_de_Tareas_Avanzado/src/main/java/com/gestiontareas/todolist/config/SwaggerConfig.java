35ab085aa689bceb27b3f74f72bdfc4e0a79d737
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-21 10:51:36 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/config/SwaggerConfig.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.gestiontareas.todolist.config;
 
 import org.springframework.context.annotation.Configuration;
 
+// Clase de configuración para Swagger, aunque en este caso no se está utilizando Swagger, sino OpenAPI (Swagger 3.0), por lo que esta clase puede estar vacía o ser eliminada si no se va a usar Swagger 2.x
 @Configuration	// Indica que esta clase es una clase de configuración de Spring
 public class SwaggerConfig {
 
