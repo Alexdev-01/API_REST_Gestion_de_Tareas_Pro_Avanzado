53ba1baae9a9e415da1650b8d04896847a44b00b
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-22 11:28:17 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/request/UsuarioRequestDTO.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/request/UsuarioRequestDTO.java
@@ -4,13 +4,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-@Data
+// Representa lo que entra por POST o PUT para los usuarios
+@Data	// Clase DTO para las solicitudes de creación o actualización de usuarios
 public class UsuarioRequestDTO {
 
-	@NotBlank(message = "El nombre es obligatorio")
+	@NotBlank(message = "El nombre es obligatorio")	// El nombre no puede estar vacío
 	private String nombre;
 
-	@NotBlank(message = "El email es obligatorio")
-	@Email(message = "El email debe tener un formato válido")
+	@NotBlank(message = "El email es obligatorio")	// El email no puede estar vacío
+	@Email(message = "El email debe tener un formato válido")	// El email debe tener un formato válido
 	private String email;
 }
