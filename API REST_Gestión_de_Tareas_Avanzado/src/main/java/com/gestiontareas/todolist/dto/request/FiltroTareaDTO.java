26b2ba73dae16feddc2e36009a4e4f404a70546e
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-22 11:27:55 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/request/FiltroTareaDTO.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/request/FiltroTareaDTO.java
@@ -4,6 +4,7 @@ import com.gestiontareas.todolist.model.EstadoTarea;
 
 import lombok.Data;
 
+// DTO para filtrar tareas según ciertos criterios, como estado, usuario asignado o título
 @Data  // Anotación de Lombok para generar getters, setters, toString, equals y hashCode automáticamente
 public class FiltroTareaDTO {
 	private EstadoTarea estado;
