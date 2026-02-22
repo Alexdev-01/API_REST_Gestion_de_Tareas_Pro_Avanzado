432b9ef87cceeead97c8fa4b76692c6e1aae984d
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-22 11:27:22 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/controller/UsuarioController.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/controller/UsuarioController.java
@@ -18,26 +18,30 @@ import com.gestiontareas.todolist.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("/api/usuarios")
-@RequiredArgsConstructor
+// Clase de controlador REST que maneja las solicitudes relacionadas con los usuarios
+@RestController // Indica que esta clase es un controlador REST de Spring
+@RequestMapping("/api/usuarios") // Define la ruta base para las solicitudes de este controlador
+@RequiredArgsConstructor // Genera un constructor con los campos finales
 public class UsuarioController {
 
 	private final UsuarioService usuarioService;
 
-	@PostMapping
+	// Método para crear un nuevo usuario
+	@PostMapping	// @PostMapping indica que este método maneja solicitudes HTTP POST a la ruta "/api/usuarios"
 	public ResponseEntity<UsuarioResponseDTO> crearUsuario(@Valid @RequestBody UsuarioRequestDTO request) {
 		UsuarioResponseDTO response = usuarioService.crearUsuario(request);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
-	@GetMapping("/{id}")
+	// Método para obtener un usuario por su ID
+	@GetMapping("/{id}")	// @PathVariable indica que el valor de la variable de ruta se mapea al parámetro id
 	public ResponseEntity<UsuarioResponseDTO> obtenerUsuarioPorId(@PathVariable Long id) {
 		UsuarioResponseDTO response = usuarioService.obtenerUsuarioPorId(id);
 		return ResponseEntity.ok(response);
 	}
 	
-	@GetMapping
+	// Método para obtener un usuario por su nombre de usuario
+	@GetMapping	// Método para listar todos los usuarios
 	public ResponseEntity<List<UsuarioResponseDTO>> listarUsuarios() {
 		List<UsuarioResponseDTO> usuarios = usuarioService.listarUsuarios();
 		return ResponseEntity.ok(usuarios);
