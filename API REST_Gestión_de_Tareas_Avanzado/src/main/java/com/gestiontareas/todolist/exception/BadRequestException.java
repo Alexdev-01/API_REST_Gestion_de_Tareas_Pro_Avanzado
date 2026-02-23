5831b426ab19f9a62cc3b8a3beab650e8d65df02
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-23 10:56:27 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/BadRequestException.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/BadRequestException.java
@@ -1,5 +1,6 @@
 package com.gestiontareas.todolist.exception;
 
+// Excepción personalizada para manejar errores de solicitud incorrecta (HTTP 400 Bad Request)
 public class BadRequestException extends RuntimeException{
 	
 	public BadRequestException (String messager) {
