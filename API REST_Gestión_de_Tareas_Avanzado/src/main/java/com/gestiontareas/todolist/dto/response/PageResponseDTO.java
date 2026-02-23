555539c47b003f1e0741aa53b0449936c08a76ce
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-23 10:55:42 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/response/PageResponseDTO.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/dto/response/PageResponseDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+// Clase genérica para representar una respuesta paginada, con información sobre el contenido, la página actual, el tamaño de página, el total de elementos y el total de páginas
 @Data  // Anotación de Lombok para generar getters, setters, toString, equals y hashCode automáticamente
 @AllArgsConstructor  // Anotación de Lombok para generar un constructor con todos los argumentos
 public class PageResponseDTO<T> {
