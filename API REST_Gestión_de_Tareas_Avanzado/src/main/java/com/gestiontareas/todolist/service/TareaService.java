83635204934ff5a78737c2d1950ec7bb33aa597a
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-24 11:08:41 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/TareaService.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/TareaService.java
@@ -10,18 +10,24 @@ import com.gestiontareas.todolist.dto.response.TareaResponseDTO;
 import com.gestiontareas.todolist.model.EstadoTarea;
 import com.gestiontareas.todolist.model.Tarea;
 
+// Interfaz que define los métodos del servicio de tareas. Establece las operaciones que se pueden realizar sobre las tareas.
 public interface TareaService {
-	//TareaResponseDTO crear(TareaRequestDTO dto);
 
+	// Método para crear una nueva tarea, recibe un objeto TareaRequestDTO con los datos de la tarea a crear y devuelve un objeto TareaResponseDTO con los datos de la tarea creada
 	TareaResponseDTO crearTarea(TareaResponseDTO dto);
 	
+	// Método para listar las tareas de un usuario específico, recibe el ID del usuario y un objeto Pageable para la paginación, y devuelve una página de objetos TareaResponseDTO con los datos de las tareas del usuario
 	Page<TareaResponseDTO> listarTareasPorUsuario(Long usuarioId, Pageable pageable);
 	
+	// Método para actualizar una tarea existente, recibe el ID de la tarea a actualizar y un objeto TareaRequestDTO con los nuevos datos de la tarea, y devuelve un objeto TareaResponseDTO con los datos de la tarea actualizada
 	TareaResponseDTO actualizarTarea(Long id, TareaRequestDTO dto);
 	
+	// Método para eliminar una tarea, recibe el ID de la tarea a eliminar y no devuelve ningún valor
 	void elimarTarea(Long id);
 	
+	// Método para cambiar el estado de una tarea, recibe el ID de la tarea y el nuevo estado a asignar, y devuelve un objeto TareaResponseDTO con los datos de la tarea actualizada con el nuevo estado
 	TareaResponseDTO cambiarEstado(Long id, EstadoTarea nuevoEstado);
 
+	// Método para listar todas las tareas con paginación, recibe un objeto Pageable para la paginación y devuelve una página de objetos TareaResponseDTO con los datos de todas las tareas
 	Page<TareaResponseDTO> listarTodas(Pageable pageable);
 }
