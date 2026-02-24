738ad244f51d2bed8a462f61826ae8235cd7e281
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-24 11:08:12 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/UsuarioRepository.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/UsuarioRepository.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Repository;
 import com.gestiontareas.todolist.model.Usuario;
 
 @Repository // Indica que esta clase es un repositorio de Spring Data JPA
+// Al extender JpaRepository, esta interfaz hereda métodos para realizar operaciones CRUD (Create, Read, Update, Delete) y consultas personalizadas sobre la entidad Usuario, utilizando Long como tipo de dato para la clave primaria
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	// Método personalizado para encontrar un usuario por su email, devuelve un Optional<Usuario> que puede contener un usuario si se encuentra uno con el email especificado, o estar vacío si no se encuentra ningún usuario con ese email
     Optional<Usuario> findByEmail(String email);
 
 }
