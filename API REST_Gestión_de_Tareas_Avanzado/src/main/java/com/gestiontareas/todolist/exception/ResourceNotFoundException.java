727a331e55f4a6fdcf0f882a3c26d65ee00af5bc
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-24 10:28:19 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/ResourceNotFoundException.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/exception/ResourceNotFoundException.java
@@ -1,5 +1,6 @@
 package com.gestiontareas.todolist.exception;
 
+// Excepción personalizada para manejar errores de recurso no encontrado (HTTP 404 Not Found)
 public class ResourceNotFoundException extends RuntimeException {
 	
 	public ResourceNotFoundException(String message) { 
