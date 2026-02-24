aacc5dac17c118a8df1b8e0ce4f8b6fcbdddf179
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-24 11:08:00 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/TareaRepository.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/repository/TareaRepository.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Repository;
 import com.gestiontareas.todolist.model.Tarea;
 
 @Repository // Indica que esta clase es un repositorio de Spring Data JPA
+// Al extender JpaRepository, esta interfaz hereda métodos para realizar operaciones CRUD (Create, Read, Update, Delete) y consultas personalizadas sobre la entidad Tarea, utilizando Long como tipo de dato para la clave primaria
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
 	
+	//List tareas por usuario con paginación
 	Page<Tarea> findByUsuarioId(Long usuarioId, Pageable pageable);
 	
 	//List tareas por usuario y estado
