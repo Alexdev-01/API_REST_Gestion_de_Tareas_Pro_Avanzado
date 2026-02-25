6d041e0abc45c2bbcaa73f892fcdb0a042b01b4d
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-02-25 15:06:59 +0100

    Add files via upload

--- API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/UsuarioService.java
+++ API REST_Gestión_de_Tareas_Avanzado/src/main/java/com/gestiontareas/todolist/service/UsuarioService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.gestiontareas.todolist.dto.request.UsuarioRequestDTO;
 import com.gestiontareas.todolist.dto.response.UsuarioResponseDTO;
 
+// Interfaz que define los métodos del servicio de usuarios. Establece las operaciones que se pueden realizar sobre los usuarios.
 public interface UsuarioService {
 
 	UsuarioResponseDTO crearUsuario(UsuarioRequestDTO request);
